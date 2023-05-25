72ab731c0ea470ebbe47aa4964d41a9cd3df06b4
Erick Hernandez <yerickk8@gmail.com>
2023-05-25 10:19:53 -0600

    Auth UI

--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import axios from "axios";
-import { useState } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
 import { useRegisterModal } from "@/app/hooks/useRegisterModal";
+
 import { Modal } from "./Modal";
 import { Heading } from "../Heading";
+import Input from "../inputs/Input";
+import Button from "../Button";
 
 export const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -34,7 +41,7 @@ export const RegisterModal = () => {
         // loginModal.onOpen();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("Something went wrong!");
       })
       .finally(() => {
         setIsLoading(false);
@@ -44,6 +51,72 @@ export const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>
+          Already have an account?
+          <span
+            onClick={registerModal.onClose}
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+          >
+            {" "}
+            Log in
+          </span>
+        </p>
+      </div>
     </div>
   );
 
@@ -56,7 +129,7 @@ export const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
-      //   footer={footerContent}
+      footer={footerContent}
     />
   );
 };
